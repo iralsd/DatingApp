539b17aec9d909bf0a0deb3b9b187562ade2f300
DovganMariya <feya.forests.05@mail.ru>
2025-11-22 20:46:02 +0300

    added colors
    colors

---
+++ dating_app/lib/design/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+const Color primaryColor = Color(0xFFFFFFFF);
+
+const Color secondaryColor = Color(0xFFF3F2F9);
+
+const Color secondaryVariantColor = Color(0xFFDB2A6B);
+
+const Color surfaceColor = Color(0xFFFFFFFF);
