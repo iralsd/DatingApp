3bd26ff5d2ece3e9d77d9cbd548a43adee078dd5
DovganMariya <feya.forests.05@mail.ru>
2025-11-22 22:06:45 +0300

    people model

--- dating_app/lib/models/people_models.dart
+++ dating_app/lib/models/people_models.dart
@@ -4,6 +4,11 @@ class PeopleModel {
   final String age;
   final String photoUrl;
   final bool isOnline;
+  final bool isVip;
+  final String lastMessage;
+  final DateTime timestamp;
+  final int unreadCount;
+  final bool isTyping;
 
   const PeopleModel({
     required this.id,
@@ -11,5 +16,10 @@ class PeopleModel {
     required this.age,
     required this.photoUrl,
     required this.isOnline,
+    required this.isVip,
+    required this.lastMessage,
+    required this.timestamp,
+    this.unreadCount = 0,
+    this.isTyping = false,
   });
 }
