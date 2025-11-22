62cdc1fd9f553252e50984251d02d7ae52e92c17
DovganMariya <feya.forests.05@mail.ru>
2025-11-22 21:57:16 +0300

    added people model

---
+++ dating_app/lib/models/people_models.dart
@@ -0,0 +1,15 @@
+class PeopleModel {
+  final String id;
+  final String name;
+  final String age;
+  final String photoUrl;
+  final bool isOnline;
+
+  const PeopleModel({
+    required this.id,
+    required this.name,
+    required this.age,
+    required this.photoUrl,
+    required this.isOnline,
+  });
+}
